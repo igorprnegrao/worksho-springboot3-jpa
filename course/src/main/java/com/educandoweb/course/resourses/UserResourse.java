8c1212f878cad2fd6329ab14e0b25e5bddb51739
igorprnegrao <neurocientista@gmail.com>
2025-01-05 01:18:11 -0300

    comments mapping and restController

--- course/src/main/java/com/educandoweb/course/resourses/UserResourse.java
+++ course/src/main/java/com/educandoweb/course/resourses/UserResourse.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.educandoweb.course.entities.User;
 
 
-@RestController //anotation = recurso web / controlado por um recurso rest
-@RequestMapping(value = "/users") //  nome para o recurso /caminho para entidade users 
+@RestController //@RestController é melhor usado quando você quer retornar 
+// dados (por exemplo, JSON ou XML)
+@RequestMapping(value = "/users") // é usada para mapear URLs para métodos 
+//específicos em controladores 
 public class UserResourse {
 	
-	@GetMapping  // ??
+	@GetMapping  // annotation = mapeia apenas requisições do tipo GET
 	public ResponseEntity<User> findAll() {
+		
 		User u = new User(1L, "Maria", "maria@gmail.com", "(91)9999-9999", "123456");
 		
 		return ResponseEntity.ok().body(u);
