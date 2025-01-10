a43556d597795f0d5c45de8a62e03836381f95ac
igorprnegrao <neurocientista@gmail.com>
2025-01-10 02:56:32 -0300

    update

--- course/src/main/java/com/educandoweb/course/resourses/UserResourse.java
+++ course/src/main/java/com/educandoweb/course/resourses/UserResourse.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +53,13 @@ public class UserResourse {
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
-	}	
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		obj = userService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	
 }
